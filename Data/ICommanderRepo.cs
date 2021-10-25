cd74b0c46582e4dfea4cb6db8092feb6d7cb4e35
Chinwe Ibegbu <chinwe.ibegbu@gmail.com>
2021-10-25 16:42:15 +0100

    Added repository interface and mock implementation

---
+++ Data/ICommanderRepo.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;   // contains IEnumerable
+using Commander.Models;             // allows repo to use the Command model
+
+namespace Commander.Data
+{
+    public interface ICommanderRepo
+    {
+        IEnumerable<Command> GetAppCommands();
+        Command GetCommandById(int Id);
+    }
+}
---
+++ Data/MockCommanderRepo.cs
@@ -0,0 +1,26 @@
+﻿using Commander.Models;
+using System.Collections.Generic;
+
+namespace Commander.Data
+{
+    public class MockCommanderRepo : ICommanderRepo
+    {
+        public IEnumerable<Command> GetAppCommands()
+        {
+            var commands = new List<Command>
+            {
+                new Command{Id=0, HowTo="Find yourself", Line="Graduating", Platform="Ashesi University"},
+                new Command{Id=1, HowTo="Register for classes", Line="Graduating", Platform="University of Benin" },
+                new Command{Id=2, HowTo="Study hard", Line="Graduating", Platform="Harvard University"}
+            };
+
+            return commands;
+        }
+
+        public Command GetCommandById(int Id)
+        {
+            return new Command { Id = 0, HowTo = "Study hard", Line = "Graduating", Platform = "Ashesi University" };
+        }
+
+    }
+}
