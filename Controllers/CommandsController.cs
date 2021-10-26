7b5a05b3411fd03d4abfda6c284f8e9791d2dcf3
Chinwe Ibegbu <chinwe.ibegbu@gmail.com>
2021-10-26 10:44:07 +0100

    Added controller with mock repository instantiation

---
+++ Controllers/CommandsController.cs
@@ -0,0 +1,39 @@
+﻿using Commander.Data;
+using Commander.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Commander.Controllers
+{
+    /* You can also define a route with [Route("api/[controller]")].
+     * This automaically replaces the "[controller]" with the name of your controller class.
+     */
+
+    [Route("api/commands")]
+    [ApiController]
+    public class CommandsController : ControllerBase
+    {
+        private readonly MockCommanderRepo _repository = new MockCommanderRepo();
+
+        // GET api/commands
+        [HttpGet]
+        public ActionResult<IEnumerable<Command>> GetAllCommands()
+        {
+            var commandItems = _repository.GetAppCommands();
+
+            return Ok(commandItems);
+        }
+
+        // GET api/commands/{id}
+        [HttpGet("{id}")] 
+        public ActionResult<Command> GetCommandById(int id)
+        {
+            var commandItem = _repository.GetCommandById(id);
+
+            return Ok(commandItem);
+        }
+    }
+}
