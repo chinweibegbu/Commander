aa4378865f5e3890bdfd2bf3caa920cc32960f24
Chinwe Ibegbu <chinwe.ibegbu@gmail.com>
2021-10-26 14:32:36 +0100

    Generated initial migration

---
+++ Data/CommanderContext.cs
@@ -0,0 +1,20 @@
+﻿using Commander.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Commander.Data
+{
+    public class CommanderContext : DbContext
+    {
+        public CommanderContext(DbContextOptions<CommanderContext> opt) : base(opt)
+        {
+
+        }
+
+        // Set up the Command object DB representation
+        public DbSet<Command> Commands { get; set; }
+    }
+}
